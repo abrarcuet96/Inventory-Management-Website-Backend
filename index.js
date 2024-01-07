0ce1fb15ad188221d4aa364e250df15881b5d70a
Md Abrar Haider <abrarhaider1357@gmail.com>
2024-01-07 21:01:34 +0600

    redesigned website

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // ---------------------------------------------------------------------------------------------------
     const usersCollection = client.db('inventoryDB').collection('users');
     const shopCollection = client.db('inventoryDB').collection('shops');
